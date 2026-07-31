670edb9613092b6b52e7ac31326f363cbc1f253e
Lakshay Bajaj <lakshaybajaj@yahoo.com>
2026-07-31 22:02:51 +0530

    migrations

---
+++ src/HobbyApp.Infrastructure/Persistence/Migrations/20260731163224_AddTaskViewsAndCompletion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HobbyApp.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTaskViewsAndCompletion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "DeletedAt",
+                table: "Tasks",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCompleted",
+                table: "Tasks",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "IsCompleted",
+                table: "Tasks");
+        }
+    }
+}
